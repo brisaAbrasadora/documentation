972f960938e7216305f46dfe2ffea1968f999130
Daniel Rabago Prades <danielrabagoprades@gmail.com>
2023-10-24 13:31:26 +0200

    news added

--- navegator.js
+++ navegator.js
@@ -19,10 +19,15 @@ function changeSection(sectionName) {
     let section = document.getElementById(sectionName.toLowerCase());
     section.classList.toggle('hidden');
     let logo = document.getElementById('logo');
-    if(logo.classList.contains('hidden'))   logo.classList.remove('hidden');
-    logo.setAttribute('src', `img/${sectionName.toLowerCase()}-logo.png`)
     let title = document.getElementById('title');
-    title.innerText = `${sectionName} documentation`;
+    if(sectionName === "News") {
+        logo.classList.add('hidden');
+        title.innerText = 'Documentation';
+    } else {
+        logo.classList.remove('hidden');
+        logo.setAttribute('src', `img/${sectionName.toLowerCase()}-logo.png`)
+        title.innerText = `${sectionName} documentation`;
+    }
 }
 
 function changeCss(sectionName) {
